9e45eaeeb6d5d4b6fc4a71a6a354104cbc6e56f6
Ilia Balakirev <i.balakirev.stc@innopolis.university>
2019-01-27 01:44:00 +0300

    Сделал OutOfMemory HeapSpace и MetaSpace.

---
+++ src/lab9_GarbageCollector/MyClassLoader.java
@@ -0,0 +1,22 @@
+package lab9_GarbageCollector;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class MyClassLoader extends ClassLoader {
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        //System.out.println("Начат поиск класса: " + name);
+        try {
+            //byte[] b = Files.readAllBytes(Paths.get("./Worker.class"));
+            byte[] b = Files.readAllBytes(Paths.get("./"+name.replaceAll("^.*[.]","")+".class"));
+            return defineClass(name, b, 0, b.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return super.findClass(name);
+    }
+
+
+}
